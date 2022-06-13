37afbdfcc3dc0cd1f2a7c25fd128b7e9b059dc8b
TalitaFraga <talita.fraga@liferay.com>
2022-06-13 17:19:13 -0300

    GSTPE:52 init web modules

---
+++ modules/amf-registration/amf-registration-web/src/main/java/com/liferay/docs/mvcportlet/constants/amfRegistrationWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.docs.mvcportlet.constants;
+
+/**
+ * @author me
+ */
+public class amfRegistrationWebPortletKeys {
+
+	public static final String AMFREGISTRATIONWEB =
+		"com_liferay_docs_mvcportlet_amfRegistrationWebPortlet";
+
+}

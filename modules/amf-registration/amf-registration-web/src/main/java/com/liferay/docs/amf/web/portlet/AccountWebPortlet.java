04feeadd64db17816ee821e86fa35439cbbbb9f6
TalitaFraga <talita.fraga@liferay.com>
2022-06-15 15:16:13 -0300

    GSTPE:52 refactoring AccountWebPortlet

--- modules/amf-registration/amf-registration-web/src/main/java/com/liferay/docs/amf/web/portlet/AccountWebPortlet.java
+++ modules/amf-registration/amf-registration-web/src/main/java/com/liferay/docs/amf/web/portlet/AccountWebPortlet.java
@@ -17,10 +17,10 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=AccountWeb",
+		"javax.portlet.display-name=Account",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + AccountWebPortletKeys.ACCOUNTWEB,
+		"javax.portlet.name=" + AccountWebPortletKeys.ACCOUNT,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user"
 	},

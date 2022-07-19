48f1ca4d9d4b215cc47296ede0c2091d817fecd6
TalitaFraga <talita.fraga@liferay.com>
2022-07-19 12:09:17 -0300

    GSTPE:75 impl loginPreAction

--- modules/common-extensions/src/main/java/com/liferay/common/extensions/preaction/login/api/LoginPreActionCommonExtensionsImpl.java
+++ modules/common-extensions/src/main/java/com/liferay/common/extensions/preaction/login/api/LoginPreActionCommonExtensionsImpl.java
@@ -3,8 +3,14 @@ package com.liferay.common.extensions.preaction.login.api;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.LifecycleAction;
 import com.liferay.portal.kernel.events.LifecycleEvent;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.WebKeys;
 import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Component(
         immediate = true, property = "key=" + PropsKeys.SERVLET_SERVICE_EVENTS_PRE,
@@ -13,7 +19,24 @@ import org.osgi.service.component.annotations.Component;
 public class LoginPreActionCommonExtensionsImpl implements LifecycleAction {
 
     @Override
-    public void processLifecycleEvent(LifecycleEvent lifecycleEvent) throws ActionException {
+    public void processLifecycleEvent(LifecycleEvent lifecycleEvent) {
 
+        HttpServletRequest request = lifecycleEvent.getRequest();
+
+        ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(WebKeys.THEME_DISPLAY);
+
+        if(themeDisplay.isSignedIn()){
+            return;
+        }
+        if (!themeDisplay.getURLCurrent().equals("sample-page")){
+            return;
+        }
+        if(!themeDisplay.isSignedIn()){
+            try{
+                lifecycleEvent.getResponse().sendRedirect("hidden-page");
+            }catch (IOException ioException){
+                throw new RuntimeException(ioException);
+            }
+        }
     }
 }
--- modules/common-extensions/src/main/java/com/liferay/common/extensions/preaction/login/api/loginPreActionCommonExtensions.java
+++
@@ -1,7 +0,0 @@
-package com.liferay.common.extensions.preaction.login.api;
-
-/**
- * @author me
- */
-public interface loginPreActionCommonExtensions {
-}
